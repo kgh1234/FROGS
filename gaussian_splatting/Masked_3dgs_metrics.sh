dca0d4edf268e1873d1aea4e1f74aadc23dcf794
EunjiKim02 <now0104@naver.com>
2025-11-17 10:43:26 +0900

    metrics

--- gaussian_splatting/Masked_3dgs_metrics.sh
+++ gaussian_splatting/Masked_3dgs_metrics.sh
@@ -6,9 +6,9 @@
 
 SCENE_NAME="lerf_mask"
 ROOT="../../masked_datasets/$SCENE_NAME"
-OUTPUT_ROOT="../../output_objectgs/$SCENE_NAME"
+OUTPUT_ROOT="../../output_masked_refine_lerf/$SCENE_NAME"
 CSV_FILE="$OUTPUT_ROOT/metrics_summary_$SCENE_NAME.csv"
-SHEET_NAME="ObjectGS"
+SHEET_NAME="ablation"
 
 export CUDA_VISIBLE_DEVICES=0
 
@@ -29,7 +29,7 @@ for SCENE_PATH in "$ROOT"/*; do
         PSNR=$(grep "PSNR" metrics_tmp.log | awk '{print $3}')
         LPIPS=$(grep "LPIPS" metrics_tmp.log | awk '{print $2}')
 
-        python ../../update_sheet.py "$SHEET_NAME" "$SCENE" "$SSIM" "$PSNR" "$LPIPS" "$TRAIN_TIME" "$RENDER_TIME" "$VRAM_MAX"
+        python ../../update_sheet.py "$SHEET_NAME" "$SCENE" "$SSIM" "$PSNR" "$LPIPS" "" "$TRAIN_TIME" "$RENDER_TIME" "$VRAM_MAX"
 
 
 
